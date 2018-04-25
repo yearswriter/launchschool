de70ae8fed9d2a490a0a4e70c145ba2fc4bc6ab0
Пешеев Павел <i@pesheevpavel.ru>
2018-04-25 22:26:07 +0300

    better name.rb

--- 100_Programming_and_Back-end_Prep/12_Book:_Learn_to_Program/name.rb
+++ 100_Programming_and_Back-end_Prep/12_Book:_Learn_to_Program/name.rb
@@ -1,5 +1,5 @@
 print "What is your full name? "
-full_name = gets.chomp.capitalize.split(" ")
+full_name = gets.chomp.split(" ")
 if full_name.length<1
   puts "Hey, why so silent :("
   return
@@ -8,7 +8,9 @@ elsif full_name.length==1
 elsif full_name.length>3
   puts "Wow! You've got a lot of names!"
 end
+full_name_length=0
+full_name.each {|name| full_name_length+=name.length}  
 print "Greetings, "
-full_name.each {|name| print "#{name} "}
-puts "!\nDid I got your name right?" 
+full_name.each {|name| print "#{name.capitalize} "}
+puts "!\nDid you know there are #{full_name_length} characters in your name?" 
  
