ede741d1b136e98c7e41ef705b71baf175ccaee8
Пешеев Павел <i@pesheevpavel.ru>
2018-05-18 01:47:17 +0300

    Better modern_roman_num method This problem hurts my brain

--- 100_Programming_and_Back-end_Prep/12_Book:_Learn_to_Program/roman_num.rb
+++ 100_Programming_and_Back-end_Prep/12_Book:_Learn_to_Program/roman_num.rb
@@ -1,4 +1,6 @@
+inp =2849
 def roman_num inp
+  num = String.new
   roman_sheet ={thousand: ['M',inp/1000],
                 five_hun: ['D',inp%1000/500],
                 hundred:  ['C',inp%1000%500/100],
@@ -6,33 +8,37 @@ def roman_num inp
                 ten:      ['X',inp%1000%500%100%50/10],
                 five:     ['V',inp%1000%500%100%50%10/5],
                 one:      ['I',inp%1000%500%100%50%10%5]}
-  roman_sheet.each {|k,v| print v[0]*v[1]}
-  puts
+  roman_sheet.each {|k,v| num+= v[0]*v[1]}
+  return num
 end
-print "Which Roman number you wish to create: "
-inp = gets.chomp.to_i
-roman_num inp
+#print "Which Roman number you wish to create: "
+#inp = gets.chomp.to_i
+puts roman_num inp
+
+
 def modern_roman_num inp  #https://en.wikipedia.org/wiki/Roman_numerals
-  roman_sheet =['M',inp/1000,1000],
-               ['D',inp%1000/500,500],
-               ['C',inp%1000%500/100,100],
-               ['L',inp%1000%500%100/50,50],
-               ['X',inp%1000%500%100%50/10,10],
-               ['V',inp%1000%500%100%50%10/5,5],
-               ['I',inp%1000%500%100%50%10%5,1]
-  i=0            
-  while i < roman_sheet.length.to_i
-    #print roman_sheet[i][0]*roman_sheet[i][1]
-    if roman_sheet[i][1]>(roman_sheet[i-1][2]-roman_sheet[i][1])
-      print roman_sheet[i][0]*(roman_sheet[i-1][2]-roman_sheet[i][1])
-      print roman_sheet[i-1][0]*roman_sheet[i-1][1]
+  num = String.new
+  roman_sheet =[['M',inp/1000,1000],
+                ['D',inp%1000/500,500],
+                ['C',inp%1000%500/100,100],  
+                ['L',inp%1000%500%100/50,50],
+                ['X',inp%1000%500%100%50/10,10],
+                ['V',inp%1000%500%100%50%10/5,5],
+                ['I',inp%1000%500%100%50%10%5,1]]
+  #..leaving alone thousands from our shenanigans, since there are no roman nums bigger
+  num+= roman_sheet[0][0]*roman_sheet[0][1]
+  index =1
+  #..
+  while index<roman_sheet.length
+    if roman_sheet[index][1]<4
+      num+= roman_sheet[index][0]*roman_sheet[index][1]
     else
-      print roman_sheet[i][0]*roman_sheet[i][1]
+      num+= roman_sheet[index][0] + roman_sheet[index-1][0]
     end
-    i+=1
+    index +=1
   end
-  puts
+  return num
 end
-print "Which Roman number you wish to create: "
-inp = gets.chomp.to_i
-modern_roman_num inp
+#print "Which Roman number you wish to create: "
+#inp = gets.chomp.to_i
+puts modern_roman_num inp
