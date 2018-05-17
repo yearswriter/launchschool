a493e39893801007eafaabd4a71f4f6d0bf2992a
Пешеев Павел <i@pesheevpavel.ru>
2018-05-18 02:00:54 +0300

    better modern_roman_num Cannot figure out what to do 9-s

--- 100_Programming_and_Back-end_Prep/12_Book:_Learn_to_Program/roman_num.rb
+++ 100_Programming_and_Back-end_Prep/12_Book:_Learn_to_Program/roman_num.rb
@@ -1,4 +1,3 @@
-inp =2849
 def roman_num inp
   num = String.new
   roman_sheet ={thousand: ['M',inp/1000],
@@ -11,20 +10,20 @@ def roman_num inp
   roman_sheet.each {|k,v| num+= v[0]*v[1]}
   return num
 end
-#print "Which Roman number you wish to create: "
-#inp = gets.chomp.to_i
+print "Which Roman number you wish to create: "
+inp = gets.chomp.to_i
 puts roman_num inp
 
 
 def modern_roman_num inp  #https://en.wikipedia.org/wiki/Roman_numerals
   num = String.new
-  roman_sheet =[['M',inp/1000,1000],
-                ['D',inp%1000/500,500],
-                ['C',inp%1000%500/100,100],  
-                ['L',inp%1000%500%100/50,50],
-                ['X',inp%1000%500%100%50/10,10],
-                ['V',inp%1000%500%100%50%10/5,5],
-                ['I',inp%1000%500%100%50%10%5,1]]
+  roman_sheet =[['M',inp/1000],
+                ['D',inp%1000/500],
+                ['C',inp%1000%500/100],  
+                ['L',inp%1000%500%100/50],
+                ['X',inp%1000%500%100%50/10],
+                ['V',inp%1000%500%100%50%10/5],
+                ['I',inp%1000%500%100%50%10%5]]
   #..leaving alone thousands from our shenanigans, since there are no roman nums bigger
   num+= roman_sheet[0][0]*roman_sheet[0][1]
   index =1
@@ -32,13 +31,16 @@ def modern_roman_num inp  #https://en.wikipedia.org/wiki/Roman_numerals
   while index<roman_sheet.length
     if roman_sheet[index][1]<4
       num+= roman_sheet[index][0]*roman_sheet[index][1]
-    else
+    elsif roman_sheet[index][1]%9==0
+      num +=roman_sheet[index][0] + roman_sheet[index-1][0]
+      index +=1
+    else 
       num+= roman_sheet[index][0] + roman_sheet[index-1][0]
     end
     index +=1
   end
   return num
 end
-#print "Which Roman number you wish to create: "
-#inp = gets.chomp.to_i
+print "Which Roman number you wish to create: "
+inp = gets.chomp.to_i
 puts modern_roman_num inp
