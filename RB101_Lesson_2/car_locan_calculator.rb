26d006f3f1a5cf711aaa00b14eba9c5b59686534
Paul Windows <i@pesheevpavel.ru>
2019-07-19 23:57:51 +0300

    Half way through loan processing - I really made this too hard on myself, it shows in smelly code

--- RB101_Lesson_2/car_locan_calculator.rb
+++ RB101_Lesson_2/car_locan_calculator.rb
@@ -4,13 +4,15 @@ CONFIG      = YAML.load_file('loan_calc_config.yml')
 TARIFS      = CONFIG['tarifs']
 ID_METHODS  = CONFIG['id_methods']
 PROMTS      = CONFIG['promts']
-NAME = "LONLGOOGNGONGNEMAR"
+CURRENT_CONFIG = {}
+CURRENT_CONFIG['name']='Stranger'
 
 def quit()
   puts PROMTS['confirm_quit']
   if gets.chomp.downcase.include?("y")
     bye = sprintf(
-      "#{PROMTS['top_line']}|#{PROMTS['bye']}%-32s|\n#{PROMTS['bot_line']}", NAME.downcase.capitalize + "!")
+      "#{PROMTS['top_line']}|#{PROMTS['bye']}%-32s|\n#{PROMTS['bot_line']}",
+      CURRENT_CONFIG['name'].to_s + "!")
     puts bye
     true
   else
@@ -19,27 +21,89 @@ def quit()
   end
 end
 
+def ask_name
+  input=''
+  loop do
+    puts PROMTS['enter_name']
+    input = gets.chomp.downcase.capitalize
+    if input.empty?
+      puts PROMTS['invalid_answer']
+    else
+      break
+    end
+  end
+  return input
+end
+
 def list_tarifs
   TARIFS.each do |tarif_name,tarif_parameters|
     puts "Tarif name (c): " + tarif_name.to_s
     puts sprintf("APR: %+8s%%",tarif_parameters['apr']*100)
-    puts sprintf("Mimimum loan amount: %+8s$",tarif_parameters['min_loan'])
-    puts sprintf("Maximum loan amount: %+8s$",tarif_parameters['max_loand'])
+    puts sprintf("Mimimum loan amount: %8d$",tarif_parameters['min_loan'])
+    puts sprintf("Maximum loan amount: %8d$",tarif_parameters['max_loan'])
     puts sprintf("APR: %+8s months",tarif_parameters['max_duration']*12)
   end
-  return false
+  false
+end
+
+def enter_id
+  method = ''
+  value  = ''
+  loop do
+    puts PROMTS['promt_id_method']
+    method = gets.chomp.downcase
+    if ID_METHODS[method]
+      puts "Now enter your #{method} itself."
+      value = gets.chomp.downcase
+      check = Regexp.new(ID_METHODS[method])
+      puts value.match(check)
+      break if value.match(check)
+    else
+      puts PROMTS['invalid_answer']
+    end
+  end
+  return [method,value]
+end
+
+def select_tarifs
+  tarif={}
+  list_tarifs
+  loop do
+    puts "Enter name of desirable tarif:"
+    input = gets.chomp.downcase
+    if TARIFS[input]
+      tarif = TARIFS[input]
+      break
+    else
+      puts PROMTS['invalid_answer']
+    end
+  end
+  return tarif
+end
+
+def process_loan
+  CURRENT_CONFIG['name'] = ask_name
+  method,value = enter_id
+  CURRENT_CONFIG[method] = value
+  CURRENT_CONFIG['tarif'] = select_tarifs
+  puts "#{CURRENT_CONFIG['name']}, please, enter your desireable loan summ:"
+  loan = gets.chomp.to_f
+  if  loan <= CURRENT_CONFIG['tarif']['max_loan'] && loan >= CURRENT_CONFIG['tarif']['min_loan']
+    CURRENT_CONFIG['loan'] = loan
+    CURRENT_CONFIG['apr'] = CURRENT_CONFIG['tarif']['apr']
+  end
 end
 
 def resque_user(input)
   case input
   when 's'
-    puts "call tarif selection method"
+    process_loan
   when 'l'
     list_tarifs
   when 'h'
     puts PROMTS['help']
   when 'c'
-    puts "print current configuration"
+    puts CURRENT_CONFIG
   when 'q'
     quit()
   else
@@ -51,11 +115,9 @@ puts PROMTS['hi']
 puts PROMTS['help']
 loop do # main loop
   input = gets.chomp
-  if resque_user(input)
-    sprintf "#{PROMTS['top_line']}\n|#{PROMTS['bye']}%-41s|\n#{PROMTS['bot_line']}", "NAME!"
-    break
-  end
+  break if resque_user(input)
 end
+
 =begin
   General plan:
   Make config with 3 different (named) tarif plans for loans
