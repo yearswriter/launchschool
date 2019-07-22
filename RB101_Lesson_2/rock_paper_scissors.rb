aacc315e534b91f33d175b374597633d036a8562
Paul Windows <i@pesheevpavel.ru>
2019-07-22 17:56:01 +0300

    wrote rock paper sciccors before watching, now to lesson

---
+++ RB101_Lesson_2/rock_paper_scissors.rb
@@ -0,0 +1,41 @@
+CHOOSE = ['rock', 'scissors', 'paper']
+
+def validate(choice)
+  validated_choice = CHOOSE.find do |x|
+    x.start_with?(choice[/^[A-z]/] || x == choice)
+  end
+  if validated_choice.nil?
+    print "Wrong input! "
+    return promt_for_choice
+  end
+  validated_choice
+end
+
+def promt_for_choice
+  print "Choose (r)ock, (p)aper or (s)cissors: "
+  choice = gets.chomp.downcase
+  validate(choice)
+end
+
+def make_choice
+  CHOOSE[rand(2)]
+end
+
+def show_winner(player_a, player_b)
+  puts "First player chooses: " + player_a
+  puts "Second player chooses: " + player_b
+  a_weight = CHOOSE.index(player_a)
+  b_weight = CHOOSE.index(player_b)
+  if a_weight == b_weight
+    "Draw"
+  elsif a_weight == b_weight - 1
+    "First player won!"
+  else
+    "Second player won!"
+  end
+end
+
+human = promt_for_choice
+pc = make_choice
+
+puts show_winner(human, pc)
