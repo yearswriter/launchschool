bb830a55019eea7c93f860ec5793f7acdfe0e3a7
Paul Windows <i@pesheevpavel.ru>
2019-07-24 04:12:42 +0300

    Tea time! Also relax a bit before hard problems and another 20 of 101-109 small problems

--- RB101_Lesson_3/practice_problems_medium_1.rb
+++ RB101_Lesson_3/practice_problems_medium_1.rb
@@ -33,9 +33,125 @@ begin
     factors
   end
 
+  p "Input number to factor:"
   a = gets.chomp.to_i
   p factors(a)
 rescue => er
   p er
-  p factors_fixed(a)
+  puts factors_fixed(a)
 end
+
+# Question 4
+def rolling_buffer1(buffer, max_buffer_size, new_element)
+  buffer << new_element
+  buffer.shift if buffer.size > max_buffer_size
+  buffer
+end
+
+def rolling_buffer2(input_array, max_buffer_size, new_element)
+  buffer = input_array + [new_element]
+  buffer.shift if buffer.size > max_buffer_size
+  buffer
+end
+puts "initial buffer1:"
+p buffer1=[]
+puts "initial buffer2:"
+p buffer2=[]
+result1=''
+result2=''
+
+3.times do |number|
+  result1 = rolling_buffer1(buffer1, 2, number)
+  result2 = rolling_buffer2(buffer2, 2, number)
+end
+puts "3.times { | number | rolling_buffer1( buffer1, 2, number ) } returns #{3.times { | number | rolling_buffer1( buffer1, 2, number ) }}"
+p "Buffer itself: #{buffer1}"
+puts "3.times { | number | rolling_buffer2( buffer2, 2, number ) } returns #{3.times { | number | rolling_buffer2( buffer2, 2, number ) }}"
+p "Buffer itself: #{buffer2}"
+puts "Also, curious note: accumulating result of both in separate variables differs: result1: #{result1}, result2: #{result2}"
+
+buffer1=[]
+buffer2=[]
+result1=''
+result2=''
+
+3.times do |number|
+  p "--"
+  p rolling_buffer1(buffer1, 2, number)
+  p rolling_buffer2(buffer2, 2, number)
+end
+
+# Question 5
+
+begin
+  limit = 15
+
+  def fib(first_num, second_num)
+    while first_num + second_num < limit
+      sum = first_num + second_num
+      first_num = second_num
+      second_num = sum
+    end
+    sum
+  end
+
+  def fib_fixed(first_num, second_num, limit)
+    while first_num + second_num < limit
+      sum = first_num + second_num
+      first_num = second_num
+      second_num = sum
+    end
+    sum
+  end
+
+  result = fib(0, 1)
+  puts "result is #{result}"
+rescue => er
+  p er
+  result = fib_fixed(0, 1, 15)
+  puts "fixed result is #{result}"
+end
+
+# Question 6
+
+answer = 42
+
+def mess_with_it(some_number)
+  some_number += 8
+end
+
+new_answer = mess_with_it(answer)
+
+p answer - 8 == 34
+
+# Question 7
+
+p munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+def mess_with_demographics(demo_hash)
+  demo_hash.values.each do |family_member| #map would've saved the family or just storing argument in internal hash
+    family_member["age"] += 42
+    family_member["gender"] = "other"
+  end
+end
+
+mess_with_demographics(munsters)
+
+p munsters
+
+#Question 9
+def foo(param = "no")
+  "yes"
+end
+
+def bar(param = "no")
+  param == "no" ? "yes" : "no"
+end
+
+p bar(foo) == "no"
---
+++ RB101_Lesson_3/practice_problems_medium_2.rb
@@ -0,0 +1,239 @@
+# Question 1
+def fun_with_ids
+  a_outer = 42
+  b_outer = "forty two"
+  c_outer = [42]
+  d_outer = c_outer[0]
+
+  a_outer_id = a_outer.object_id
+  b_outer_id = b_outer.object_id
+  c_outer_id = c_outer.object_id
+  d_outer_id = d_outer.object_id
+
+  puts "a_outer is #{a_outer} with an id of: #{a_outer_id} before the block."
+  puts "b_outer is #{b_outer} with an id of: #{b_outer_id} before the block."
+  puts "c_outer is #{c_outer} with an id of: #{c_outer_id} before the block."
+  puts "d_outer is #{d_outer} with an id of: #{d_outer_id} before the block."
+  puts
+
+  1.times do
+    a_outer_inner_id = a_outer.object_id
+    b_outer_inner_id = b_outer.object_id
+    c_outer_inner_id = c_outer.object_id
+    d_outer_inner_id = d_outer.object_id
+
+    puts "a_outer id was #{a_outer_id} before the block and is: #{a_outer_inner_id} inside the block."
+    puts "b_outer id was #{b_outer_id} before the block and is: #{b_outer_inner_id} inside the block."
+    puts "c_outer id was #{c_outer_id} before the block and is: #{c_outer_inner_id} inside the block."
+    puts "d_outer id was #{d_outer_id} before the block and is: #{d_outer_inner_id} inside the block."
+    puts
+
+    a_outer = 22
+    b_outer = "thirty three"
+    c_outer = [44]
+    d_outer = c_outer[0]
+
+    puts "a_outer inside after reassignment is #{a_outer} with an id of: #{a_outer_id} before and: #{a_outer.object_id} after."
+    puts "b_outer inside after reassignment is #{b_outer} with an id of: #{b_outer_id} before and: #{b_outer.object_id} after."
+    puts "c_outer inside after reassignment is #{c_outer} with an id of: #{c_outer_id} before and: #{c_outer.object_id} after."
+    puts "d_outer inside after reassignment is #{d_outer} with an id of: #{d_outer_id} before and: #{d_outer.object_id} after."
+    puts
+
+
+    a_inner = a_outer
+    b_inner = b_outer
+    c_inner = c_outer
+    d_inner = c_inner[0]
+
+    a_inner_id = a_inner.object_id
+    b_inner_id = b_inner.object_id
+    c_inner_id = c_inner.object_id
+    d_inner_id = d_inner.object_id
+
+    puts "a_inner is #{a_inner} with an id of: #{a_inner_id} inside the block (compared to #{a_outer.object_id} for outer)."
+    puts "b_inner is #{b_inner} with an id of: #{b_inner_id} inside the block (compared to #{b_outer.object_id} for outer)."
+    puts "c_inner is #{c_inner} with an id of: #{c_inner_id} inside the block (compared to #{c_outer.object_id} for outer)."
+    puts "d_inner is #{d_inner} with an id of: #{d_inner_id} inside the block (compared to #{d_outer.object_id} for outer)."
+    puts
+  end
+
+  puts "a_outer is #{a_outer} with an id of: #{a_outer_id} BEFORE and: #{a_outer.object_id} AFTER the block."
+  puts "b_outer is #{b_outer} with an id of: #{b_outer_id} BEFORE and: #{b_outer.object_id} AFTER the block."
+  puts "c_outer is #{c_outer} with an id of: #{c_outer_id} BEFORE and: #{c_outer.object_id} AFTER the block."
+  puts "d_outer is #{d_outer} with an id of: #{d_outer_id} BEFORE and: #{d_outer.object_id} AFTER the block."
+  puts
+
+  puts "a_inner is #{a_inner} with an id of: #{a_inner_id} INSIDE and: #{a_inner.object_id} AFTER the block." rescue puts "ugh ohhhhh"
+  puts "b_inner is #{b_inner} with an id of: #{b_inner_id} INSIDE and: #{b_inner.object_id} AFTER the block." rescue puts "ugh ohhhhh"
+  puts "c_inner is #{c_inner} with an id of: #{c_inner_id} INSIDE and: #{c_inner.object_id} AFTER the block." rescue puts "ugh ohhhhh"
+  puts "d_inner is #{d_inner} with an id of: #{d_inner_id} INSIDE and: #{d_inner.object_id} AFTER the block." rescue puts "ugh ohhhhh"
+end
+
+fun_with_ids
+
+# Question 2
+def fun_with_ids
+  a_outer = 42
+  b_outer = "forty two"
+  c_outer = [42]
+  d_outer = c_outer[0]
+
+  a_outer_id = a_outer.object_id
+  b_outer_id = b_outer.object_id
+  c_outer_id = c_outer.object_id
+  d_outer_id = d_outer.object_id
+
+  puts "a_outer is #{a_outer} with an id of: #{a_outer_id} before the block."
+  puts "b_outer is #{b_outer} with an id of: #{b_outer_id} before the block."
+  puts "c_outer is #{c_outer} with an id of: #{c_outer_id} before the block."
+  puts "d_outer is #{d_outer} with an id of: #{d_outer_id} before the block.
+
+"
+
+  an_illustrative_method(a_outer, b_outer, c_outer, d_outer, a_outer_id, b_outer_id, c_outer_id, d_outer_id)
+
+
+  puts "a_outer is #{a_outer} with an id of: #{a_outer_id} BEFORE and: #{a_outer.object_id} AFTER the method call."
+  puts "b_outer is #{b_outer} with an id of: #{b_outer_id} BEFORE and: #{b_outer.object_id} AFTER the method call."
+  puts "c_outer is #{c_outer} with an id of: #{c_outer_id} BEFORE and: #{c_outer.object_id} AFTER the method call."
+  puts "d_outer is #{d_outer} with an id of: #{d_outer_id} BEFORE and: #{d_outer.object_id} AFTER the method call.
+
+"
+
+  puts "a_inner is #{a_inner} with an id of: #{a_inner_id} INSIDE and: #{a_inner.object_id} AFTER the method." rescue puts "ugh ohhhhh"
+  puts "b_inner is #{b_inner} with an id of: #{b_inner_id} INSIDE and: #{b_inner.object_id} AFTER the method." rescue puts "ugh ohhhhh"
+  puts "c_inner is #{c_inner} with an id of: #{c_inner_id} INSIDE and: #{c_inner.object_id} AFTER the method." rescue puts "ugh ohhhhh"
+  puts "d_inner is #{d_inner} with an id of: #{d_inner_id} INSIDE and: #{d_inner.object_id} AFTER the method.
+
+" rescue puts "ugh ohhhhh"
+end
+
+
+def an_illustrative_method(a_outer, b_outer, c_outer, d_outer, a_outer_id, b_outer_id, c_outer_id, d_outer_id)
+  a_outer_inner_id = a_outer.object_id
+  b_outer_inner_id = b_outer.object_id
+  c_outer_inner_id = c_outer.object_id
+  d_outer_inner_id = d_outer.object_id
+
+  puts "a_outer id was #{a_outer_id} before the method and is: #{a_outer.object_id} inside the method."
+  puts "b_outer id was #{b_outer_id} before the method and is: #{b_outer.object_id} inside the method."
+  puts "c_outer id was #{c_outer_id} before the method and is: #{c_outer.object_id} inside the method."
+  puts "d_outer id was #{d_outer_id} before the method and is: #{d_outer.object_id} inside the method.
+
+"
+
+  a_outer = 22
+  b_outer = "thirty three"
+  c_outer = [44]
+  d_outer = c_outer[0]
+
+  puts "a_outer inside after reassignment is #{a_outer} with an id of: #{a_outer_id} before and: #{a_outer.object_id} after."
+  puts "b_outer inside after reassignment is #{b_outer} with an id of: #{b_outer_id} before and: #{b_outer.object_id} after."
+  puts "c_outer inside after reassignment is #{c_outer} with an id of: #{c_outer_id} before and: #{c_outer.object_id} after."
+  puts "d_outer inside after reassignment is #{d_outer} with an id of: #{d_outer_id} before and: #{d_outer.object_id} after.
+
+"
+
+
+  a_inner = a_outer
+  b_inner = b_outer
+  c_inner = c_outer
+  d_inner = c_inner[0]
+
+  a_inner_id = a_inner.object_id
+  b_inner_id = b_inner.object_id
+  c_inner_id = c_inner.object_id
+  d_inner_id = d_inner.object_id
+
+  puts "a_inner is #{a_inner} with an id of: #{a_inner_id} inside the method (compared to #{a_outer.object_id} for outer)."
+  puts "b_inner is #{b_inner} with an id of: #{b_inner_id} inside the method (compared to #{b_outer.object_id} for outer)."
+  puts "c_inner is #{c_inner} with an id of: #{c_inner_id} inside the method (compared to #{c_outer.object_id} for outer)."
+  puts "d_inner is #{d_inner} with an id of: #{d_inner_id} inside the method (compared to #{d_outer.object_id} for outer).
+
+"
+end
+
+fun_with_ids
+
+# Question 3
+
+def tricky_method(a_string_param, an_array_param)
+  puts "Passed value in the param \"a_string_param\" with value of #{a_string_param} and id of #{a_string_param.object_id}"
+  puts "Passed value in the param \"an_array_param\" with value of #{an_array_param} and id of #{an_array_param.object_id}"
+  a_string_param += "rutabaga" # << will make it behave like an array below (change initial object)
+  puts "Changed value in the param \"a_string_param\" with value of #{a_string_param} and id of #{a_string_param.object_id}"
+  an_array_param << "rutabaga" # += will make it behave like a string above (creat new local object and change it instead)
+  puts "Changed value in the param \"an_array_param\" with value of #{an_array_param} and id of #{an_array_param.object_id}"
+end
+
+my_string = "pumpkins"
+puts "My string value: #{my_string} with id #{my_string.object_id}"
+my_array = ["pumpkins"]
+puts "My array value: #{my_array} with id #{my_array.object_id}"
+tricky_method(my_string, my_array)
+puts "My string looks like this now: #{my_string} with id #{my_string.object_id}"
+puts "My array looks like this now: #{my_array} with id #{my_array.object_id}"
+
+# Question 4
+def tricky_method_two(a_string_param, an_array_param)
+  a_string_param << 'rutabaga'
+  an_array_param = ['pumpkins', 'rutabaga'] #<< local variable!
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+tricky_method_two(my_string, my_array)
+
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
+
+# Question 5
+def fixed_tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param += ["rutabaga"]
+  return a_string_param, an_array_param
+end
+
+def fixed_tricky_method!(a_string_param, an_array_param)
+  saved_string_param = ''
+  saved_array_param = []
+  saved_string_param += a_string_param
+  saved_array_param += an_array_param
+  a_string_param << "rutabaga"
+  an_array_param << "rutabaga"
+  return saved_string_param, saved_array_param
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+
+result_string, result_array = fixed_tricky_method(my_string, my_array)
+
+puts
+puts "After fixed_tricky_method() my string still looks like this: #{my_string}, but method itself returns #{result_string}"
+puts "After fixed_tricky_method() my array still looks like this: #{my_array}, but method itself returns #{result_array}"
+puts
+
+saved_string, saved_array = fixed_tricky_method!(my_string, my_array)
+
+puts
+puts "After fixed_tricky_method!() my string looks like this now: #{my_string}, but method itself still returns #{saved_string}"
+puts "After fixed_tricky_method!() my array looks like this now: #{my_array}, but method itself still returns #{saved_array}"
+puts
+
+# Question 6
+def color_valid(color)
+  if color == "blue" || color == "green"
+    true
+  else
+    false
+  end
+end
+
+def fixed_color_valid(color)
+  color == "blue" || color == "green"
+end
+
+p fixed_color_valid("green") == color_valid("green")
+p fixed_color_valid("blue") == color_valid("blue")
+p fixed_color_valid("yellow") == color_valid("yellow")
