3415d2b84cb81d476065b1e2c70d28cdaff97d7b
Paul Pesheev <it@boattour.ru>
2020-02-12 00:45:09 +0300

    refactoring of tic-tac-toe

--- RB101_Lesson_6/ttt.rb
+++ RB101_Lesson_6/ttt.rb
@@ -1,79 +1,40 @@
 require 'yaml'
-require 'pry'
 CONFIG = YAML.load_file('./config.yml')
 
-def draw_on_tiles(tiles, row='mid', col='mid', value = ' ')
-  row = CONFIG["state"]["tiles"]["rows"][row][0]
-  col = CONFIG["state"]["tiles"]["cols"][col][0]
-  if tiles[row][col] == ' '
-    tiles[row][col] = value
-    return tiles
-  else
-    puts "ERROR! Tile is already taken"
-    return false
-  end
+# Drawing method + loading default tileset from config
+def draw_tile(board, row, col, value)
+  row = CONFIG['address_table']['rows'][row][0]
+  col = CONFIG['address_table']['cols'][col][0]
+  board['tileset'][row][col] = value
+  tileset = board['tileset']
+  return tileset
 end
 
-def fill_board(player,row,col,board)
-  row = CONFIG["state"]["tiles"]["rows"][row][1]
-  col = CONFIG["state"]["tiles"]["cols"][col][1]
-  if board[row][col] == 0
-    board[row][col] = player
-    return board
-  else
-    puts "ERROR! Tile is already taken"
-    return false
-  end
+# Collecting turn data method + loading default tileset from config
+def collect_turns(board, row, col, value)
+  row = CONFIG['address_table']['rows'][row][1]
+  col = CONFIG['address_table']['cols'][col][1]
+  board['game_turns'][row][col] = value
+  game_turns = board['game_turns']
+  return game_turns
 end
 
-def winner_row(board,cur_player)
-  board.map do |row|
-     row.reduce {|acc,val| acc = val if val == acc}
-  end.include?(cur_player)
+# Checking if tile still empty
+def empty?(board,row,col)
+  trow = CONFIG['address_table']['rows'][row][0]
+  tcol = CONFIG['address_table']['cols'][col][0]
+  row = CONFIG['address_table']['rows'][row][1]
+  col = CONFIG['address_table']['cols'][col][1]
+  tileset = board['tileset']
+  game_turns = board['game_turns']
+  tileset[trow][tcol].eql?(' ') & game_turns[row][col].zero?
 end
 
-def winner_col(board, cur_player)
-  return false
-end
-
-
-
-def winner?(board,cur_player)
-  winner_row(board,cur_player) || winner_col(board)
-end
-
-def turn(player,tiles,board)
-  loop do
-    system "cls"
-    puts "|> Input Player##{player} turn: row,col (left\\right\\top\\bot\\mid)|"
-    puts tiles
-    answer = gets.chomp.split(' ')
-    fill_board(player,answer[0],answer[1],board)
-    if player == 1
-      draw_on_tiles(tiles,answer[0],answer[1],'X')
-      puts tiles
-  return player if winner_row(board,player)
-      player = 2
-    else
-      draw_on_tiles(tiles,answer[0],answer[1],'O')
-      puts tiles
-  return player if winner_row(board,player)
-      player = 1
+# Checking if there any empty places on whole board
+def end?(board)
+  CONFIG['address_table']['rows'].map do |row|
+    CONFIG['address_table']['cols'].map do |col|
+      empty?(board, row, col)
     end
-  rescue StandardError => e
-    puts "\n\n|>!ERROR!ROW first COL second, please repeat your answer:|"
-    turn(player,tiles,board)
   end
-end
-
-loop do
-  tiles = CONFIG["tiles"].lines
-  board =  CONFIG["state"]["board"]
-  player = 1
-  winner = turn(player,tiles,board)
-  system "cls"
-  puts "Player##{winner} won!"
-  puts tiles
-  puts '|another game?|(y\n)'
-  return unless gets.chomp == 'y'
 end
