43ddeeffee58c811871c4e3e986867810f367572
Paul Pesheev <it@boattour.ru>
2020-02-13 01:03:47 +0300

    Rubocoped

--- RB101_Lesson_6/ttt.rb
+++ RB101_Lesson_6/ttt.rb
@@ -5,8 +5,7 @@ def draw_tile!(board, row, col, value)
   row = CONFIG['address_table']['rows'][row][0]
   col = CONFIG['address_table']['cols'][col][0]
   board['tileset'][row][col] = value
-  tileset = board['tileset']
-  return tileset
+  board['tileset']
 end
 
 # Filling turn data method + loading default tileset from config
@@ -14,26 +13,29 @@ def fill_turn!(board, row, col, value)
   row = CONFIG['address_table']['rows'][row][1]
   col = CONFIG['address_table']['cols'][col][1]
   board['player_turns'][row][col] = value
-  player_turns = board['player_turns']
-  return player_turns
+  board['player_turns']
 end
 
 # Checking if tile still empty
 def empty?(board, row, col)
-  trow = CONFIG['address_table']['rows'][row][0]
-  tcol = CONFIG['address_table']['cols'][col][0]
-  row = CONFIG['address_table']['rows'][row][1]
-  col = CONFIG['address_table']['cols'][col][1]
-  tileset = board['tileset']
+  rows = CONFIG['address_table']['rows']
+  cols = CONFIG['address_table']['cols']
+
+  tileset      = board['tileset']
   player_turns = board['player_turns']
-  tileset[trow][tcol].eql?(' ') & player_turns[row][col].zero?
+
+  trow = rows[row][0]
+  tcol = cols[col][0]
+
+  row = rows[row][1]
+  col = cols[col][1]
+
+  tileset[trow][tcol].eql?(' ') || player_turns[row][col].zero?
 end
 
 # Checking if there any empty places on whole board
 def untill_end?(board)
-  board['player_turns'].flatten.select do |t|
-    t.zero?
-  end
+  board['player_turns'].flatten.select(&:zero?)
 end
 
 # searching for winner lines
@@ -41,11 +43,11 @@ def winner_lines?(board, player)
   player_turns = board['player_turns']
 
   cols = player_turns.map do |row|
-     row.reduce {|acc, val| acc = val if val == acc}
+    row.reduce { |acc, val| val if val == acc }
   end.include?(player)
 
   rows = player_turns.transpose.map do |row|
-    row.reduce {|acc, val| acc = val if val == acc}
+    row.reduce { |acc, val| val if val == acc }
   end.include?(player)
 
   cols || rows
@@ -53,10 +55,19 @@ end
 
 # searching for winner diagonals
 def winner_dgnls?(board, player)
-  lr = board['player_turns'].eql?([[player,0,0],[0,player,0],[0,0,player]])
-  rl = board['player_turns'].eql?([[0,0,player],[0,player,0],[player,0,0]])
+  lr = board['player_turns'].eql?([[player, 0, 0],
+                                   [0, player, 0],
+                                   [0, 0, player]])
+
+  rl = board['player_turns'].eql?([
+                                    [0, 0, player],
+                                    [0, player, 0],
+                                    [player, 0, 0]
+                                  ])
+
   lr || rl
 end
+
 # ^ probably most readable and straight forward way to check win
 # Conditions, but included all others methods just for training
 # general win method
@@ -79,7 +90,7 @@ def turn!(board, player)
   return 'Wrong input' unless answer[0].match?(/top|bot|mid/)
   return 'Wrong input' unless answer[1].match?(/left|right|mid/)
   return 'Cell is taken' unless empty?(board, answer[0], answer[1])
-  #-------------------
+
   # actual turn
   case player
   when 1
@@ -106,24 +117,23 @@ end
 loop do
   CONFIG = YAML.load_file('./config.yml')
   board =  CONFIG['board']
-  [1,2].cycle { |player|
-  turn = turn!(board, player)
-  case turn
-  when 'Wrong input'
-    puts 'Wrong input'
-    redo
-  when 'Cell is taken'
-    puts 'Cell is taken'
-    redo
-  when 1..2
-    puts "Player №#{player} WON!"
-    break
-  when 'Draw'
-    puts 'IT\'S A DRAW'
-    break
+  [1, 2].cycle do |player|
+    turn = turn!(board, player)
+    case turn
+    when 'Wrong input'
+      puts 'Wrong input'
+      redo
+    when 'Cell is taken'
+      puts 'Cell is taken'
+      redo
+    when 1..2
+      puts "Player №#{player} WON!"
+      break
+    when 'Draw'
+      puts 'IT\'S A DRAW'
+      break
+    end
   end
-  # just check return of the method with all the CASES
-  }
   puts '|another game?|(y\n)'
   return unless gets.chomp == 'y'
 end
